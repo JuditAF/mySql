2b00941875e41fec3afc7e896dc8726d454479bb
JuditAF <idryscreep@gmail.com>
2024-07-02 19:59:29 +0200

    no está el ejercicio3 del tema3 ni está hecho con sentencias preparadas

--- reto3.js
+++ reto3.js
@@ -24,11 +24,14 @@ async function main() {
         // let [result] = await connection.query(sql)
         // console.log(result);                                //  Obtén todos los nombres y apellidos de los profesores y los nombres de las asignaturas que imparten
 
-        let sql = "SELECT first_name, last_name, title FROM teachers JOIN subject_teacher ON (teachers.id_teacher = subject_teacher.id_teacher)" +
-            "JOIN subjects ON (subjects.id_subjects = subject_teacher.id_subject) ORDER BY teachers.first_name";
+        let sql = "SELECT numStudents_XSubject, subjects.title, teachers.first_name, teachers.last_name, SUM(marks.id_student * marks.id_subject) AS numStudents_XSubject FROM marks)" +
+                  "INNER JOIN subjects ON (marks.id_subject = subjects.id_subjects)" +
+                  "JOIN subject_teacher ON (subjects.id_subjects = subject_teacher.id_subject)" + 
+                  "JOIN teachers ON (subject_teacher.id_teacher = teachers.id_teacher) ORDER BY subjects.title";
         let [result] = await connection.query(sql)
         console.log(result);                                //  Obtén el número total de alumnos por asignatura, el nombre de la asignatura y el nombre y apellidos del profesor que la imparte
 
+        // marks.id_student, marks.id_subject
     }
 
     catch (error) {
