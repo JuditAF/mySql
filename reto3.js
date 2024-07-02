c7e28c055d47e2eee86f56de1acd7ab329717be8
JuditAF <idryscreep@gmail.com>
2024-07-02 19:02:09 +0200

    subo ejercicio2 del tema3 termoinado

--- reto3.js
+++ reto3.js
@@ -14,10 +14,20 @@ async function main() {
 
         console.log("Conexión correcta");
 
-        let sql = "SELECT first_name, last_name, title FROM students JOIN marks ON (students.id_student = marks.id_student)" +
-            "JOIN subjects ON (marks.id_subject = subjects.id_subjects) ORDER BY students.first_name";
+        // let sql = "SELECT first_name, last_name, title FROM students JOIN marks ON (students.id_student = marks.id_student)" +
+        //     "JOIN subjects ON (marks.id_subject = subjects.id_subjects) ORDER BY students.first_name";
+        // let [result] = await connection.query(sql)
+        // console.log(result);                                // Obtén los nombres y apellidos de los alumnos y los nombres de las asignaturas en las que están apuntados
+
+        // let sql = "SELECT first_name, last_name, title FROM teachers JOIN subject_teacher ON (teachers.id_teacher = subject_teacher.id_teacher)" +
+        //     "JOIN subjects ON (subjects.id_subjects = subject_teacher.id_subject) ORDER BY teachers.first_name";
+        // let [result] = await connection.query(sql)
+        // console.log(result);                                //  Obtén todos los nombres y apellidos de los profesores y los nombres de las asignaturas que imparten
+
+        let sql = "SELECT first_name, last_name, title FROM teachers JOIN subject_teacher ON (teachers.id_teacher = subject_teacher.id_teacher)" +
+            "JOIN subjects ON (subjects.id_subjects = subject_teacher.id_subject) ORDER BY teachers.first_name";
         let [result] = await connection.query(sql)
-        console.log(result);                                // Obtén los nombres y apellidos de los alumnos y los nombres de las asignaturas en las que están apuntados
+        console.log(result);                                //  Obtén el número total de alumnos por asignatura, el nombre de la asignatura y el nombre y apellidos del profesor que la imparte
 
     }
 
