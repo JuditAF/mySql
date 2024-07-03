c31b2d34d8f1a3b8bf925babfaa69e9f8cc4c8d8
JuditAF <idryscreep@gmail.com>
2024-07-03 20:13:13 +0200

    intentando solucionar problema tipo dato año con Year no Date

--- museo.js
+++ museo.js
@@ -9,7 +9,7 @@ async function main() {
             host: "localhost",
             user: "root",
             password: "JAFsql24",
-            database: "address"
+            database: "museum"
         });
 
         console.log("Conexión correcta");
@@ -22,6 +22,14 @@ async function main() {
         // let [result] = await connection.query(sql)
         // console.log(result);                                //  Obtén el número total de alumnos por asignatura, el nombre de la asignatura y el nombre y apellidos del profesor que la imparte
 
+
+        // let sql = "ALTER TABLE piezas ADD año YEAR";           // Insertar COLUMNA en tabla students
+        // let [result] = await connection.query(sql);
+        // console.log(result);
+
+        // El Beso	Gustav Klimt	1908	Un beso dorado de dos enamorados
+        // Noche Estrellada	Vincent Van Gogh	1889	La noche estrellada es una de las imágenes más conocidas del arte moderno
+        // Cotopaxi	Frederic Edwin Church	1855	Erupciones generales
     }
 
     catch (error) {
