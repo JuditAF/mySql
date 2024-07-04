995bc96306d1818a207e85be1166422333784e46
JuditAF <idryscreep@gmail.com>
2024-07-04 12:31:04 +0200

    modificaciones en tablas

--- museo.js
+++ museo.js
@@ -22,14 +22,15 @@ async function main() {
         // let [result] = await connection.query(sql)
         // console.log(result);                                //  Obtén el número total de alumnos por asignatura, el nombre de la asignatura y el nombre y apellidos del profesor que la imparte
 
-
         // let sql = "ALTER TABLE piezas ADD año YEAR";           // Insertar COLUMNA en tabla students
         // let [result] = await connection.query(sql);
         // console.log(result);
 
-        // El Beso	Gustav Klimt	1908	Un beso dorado de dos enamorados
-        // Noche Estrellada	Vincent Van Gogh	1889	La noche estrellada es una de las imágenes más conocidas del arte moderno
-        // Cotopaxi	Frederic Edwin Church	1855	Erupciones generales
+        // let sql = "INSERT INTO piezas (nombre, año, autor, descripcion)" +
+        //           "VALUES(\"Noche Estrellada\", 1889, \"Vincent Van Gogh\", \"La noche estrellada es una de las imágenes más conocidas del arte moderno\"), (\"Cotopaxi\", 1855, \"Edwin Church\", \"Erupciones generales\"), (\"Newton\", 1805, \"William Blake\", \"El arte es el árbol de la vida. La ciencia es el árbol de la muerte\"), (\"El nacimiento de Venus\", 1484, \"Sandro Botticelli\", \"La que se sostiene entre las olas\"), (\"Júpiter y Tetis\", 1811, \"Jean Auguste Dominique Ingres\", \"Una súplica por Aquiles.\"), (\"Paolo y Francesca de Rimini\", 1863, \"Gustave Doré\", \"Las pinturas de Doré nada tienen que envidiar a sus grabados\"), (\"Relatividad\", 1953, \"M.C. Escher\", \"El mundo como algo relativo\"), (\"La joven de la perla\", 1667, \"Johannes Vermeer\", \"La Mona Lisa holandesa\"), (\"El bufón Stańczyk\", 1862, \"Jan Matejko\", \"Un payaso pensativo\"), (\"La calumnia de Apeles\", 1495, \"Sandro Botticellia\", \"Una alegoría con mucho movimiento y simbolismo\")";
+        // let [result] = await connection.query(sql);
+        // console.log(result);
+
     }
 
     catch (error) {
